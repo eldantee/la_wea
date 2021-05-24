2940e84b4f8705ef691012290c6eb198002d8455
eldantee <danteprovoste@gmail.com>
2021-05-24 17:52:29 -0400

    Initial commit

---
+++ src/profe_jorge_qlo/Profe_jorge_qlo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package profe_jorge_qlo;
+
+/**
+ *
+ * @author dante
+ */
+public class Profe_jorge_qlo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
